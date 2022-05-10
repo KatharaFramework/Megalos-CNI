46eef978b3810910181ff7c871280fe5f21891d8
Mariano Scazzariello <marianoscazzariello@gmail.com>
2022-05-10 11:11:39 +0200

    Update README

--- bgp-manager/entrypoint.sh
+++ bgp-manager/entrypoint.sh
@@ -11,14 +11,14 @@ if [ "$IS_MASTER" = "false" ]; then
   cp -f /$KATHARA_CNI_BIN_FILE $CNI_BIN_DIR/_megalos
   mv -f $CNI_BIN_DIR/_megalos $CNI_BIN_DIR/megalos
 
-  # Configure frr
+  # Configure frr as a simple BGP Speaker
   rm -Rf /etc/frr/bgpd_master.stub
   mv /etc/frr/bgpd_worker.stub /etc/frr/frr.conf
   sed -i -e "s|__NODE_IP__|$NODE_IP|g" /etc/frr/frr.conf
   while [ -z "$KATHARA_MASTER_SERVICE_HOST" ]; do sleep 1; done;
   sed -i -e "s|__SERVICE_IP__|$KATHARA_MASTER_SERVICE_HOST|g" /etc/frr/frr.conf
 else
-  # Configure frr
+  # Configure frr as a Route Reflector
   rm -Rf /etc/frr/bgpd_worker.stub
   mv /etc/frr/bgpd_master.stub /etc/frr/frr.conf
 fi
